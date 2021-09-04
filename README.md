a603bd57a1624b8112b485ac22bbfeec89d4dd7e
akyc <23010109+akyc@users.noreply.github.com>
2021-09-04 15:16:57 +0300

    v1.0

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# Описание
-Структура папок:
-dist
-    js
-src
-    js
+### Порядок работы
+
+1. Устновить LTS-версию [Node.js](https://nodejs.org/en/download/)
+2. Запустить терминал / консоль в нужной папке:
+   1. `$ git clone https://github.com/akyc/escorp-uglifier.git`
+   2. `$ npm i gulp -g`
+   3. `$ npm i`
+   4. `$ code .` `$ wstorm .` `$ subl .` _etc_
+3. Указать папки с исходниками в [gulpfile.js](../blob/master/gulpfile.js), строки 7-10, по умолчанию `src/css` и `src/js`.
+4. Воспользоваться одной из команд `$ npm run css` или `$ npm run js`
+5. Забрать результат работы из папки `dist`
--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,33 @@ const { src, dest } = require('gulp'),
     cleancss = require('gulp-clean-css'),
     sourcemaps = require('gulp-sourcemaps'),
     includeJsFiles = ['src/js/*.js'],
-    excludeJsFiles = ['!src/js/_*.*'],
-    includeCssFiles = ['src/css/*.*'],
+    excludeJsFiles = ['!src/js/_*.js'],
+    includeCssFiles = ['src/css/*.css'],
     excludeCssFiles = ['!src/css/*.min.css'],
-    destFolder = 'dist/js';
+    destFolder = 'dist';
 
 function scripts() {
     return src([...includeJsFiles, ...excludeJsFiles])
         .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
-        .pipe(dest(destFolder))
+        .pipe(dest(`${destFolder}/js`))
         .pipe(rename('app.min.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('.'))
-        .pipe(dest(destFolder));
+        .pipe(dest(`${destFolder}/js`));
 
 }
 
 function styles() {
     return src([...includeCssFiles, ...excludeCssFiles])
-        .pipe(cleancss({ level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(dest('dist/css'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.css'))
+        .pipe(cleancss({ level: { 1: { specialComments: 0 } }, format: 'beautify' }))
+        .pipe(dest(`${destFolder}/css`))
+        .pipe(rename('app.min.css'))
+        .pipe(cleancss({ level: 2 }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest(`${destFolder}/css`))
 }
 
 exports.scripts = scripts
